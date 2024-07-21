ba83682572b87ca1aaf6319fa53b8f6db7158485
taardvark <31259542+taardvark@users.noreply.github.com>
2024-07-20 20:36:30 -0500

    fix

--- install.sh
+++ install.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
-# install.sh
-pdir="ls-md5.sh"
-bin=""
+YELLOW='\033[1;33m'
+NC='\033[0m' # No Color
+script_dir=$(dirname "$(readlink -f "$0")")
 
-bin="ls-md5"
-[ -f "$HOME/git/repo/mybin/$bin" ] && rm "$HOME/git/repo/mybin/$bin"
-ln $HOME/git/repo/myscripts/$pdir/$bin $HOME/git/repo/mybin/$bin
+
+# Check if $HOME/.local/bin is in the PATH
+if [[ ":$PATH:" != *":$HOME/.local/bin:"* ]]; then
+  echo -e "${YELLOW}Warning: $HOME/.local/bin is not in your PATH${NC}"
+fi
+
+
+
+for bin in "ls-md5" ; do
+  [ -f "$HOME/.local/bin/$bin" ] && rm "$HOME/.local/bin/$bin"
+  ln "$script_dir/$bin" "$HOME/.local/bin/$bin"
+done
+
+
